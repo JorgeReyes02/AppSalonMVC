9adb2343d824aa37e5bc52d11ebefd07db743ae4
Jorge Reyes Pacheco <jorgereyeslira02@gmail.com>
2022-04-07 22:52:08 -0500

    Finalizando la navegacion por tabs

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 function iniciarApp(){
+
+    mostrarSeccion();
     //Cambia la seccion cuando se presionen los tabs
     tabs();
 
@@ -25,6 +27,14 @@ function mostrarSeccion(){
     const pasoSelector = `#paso-${paso}`;
     const seccion = document.querySelector(pasoSelector);
     seccion.classList.add('mostrar');
+    //Quita la clase de actual al tab anterior
+    const tabAnterior = document.querySelector('.actual');
+    if(tabAnterior){
+        tabAnterior.classList.remove('actual');
+    }
+    //Resalta el tab actual
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add('actual');
 }
 
 function tabs(){
