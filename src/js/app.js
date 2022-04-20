066d264724364b9ee1836197b4dfc1595b0e1528
Jorge Reyes Pacheco <jorgereyeslira02@gmail.com>
2022-04-19 20:35:19 -0500

    Seleccionar los servicios

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,13 @@ let paso = 1;
 const pasoInicial = 1;
 const pasoFinal = 3;
 
+const cita = {
+    nombre : '',
+    fecha : '',
+    hora : '',
+    servicios : []
+};
+
 
 document.addEventListener('DOMContentLoaded', function(){
     iniciarApp();
@@ -126,6 +133,9 @@ function mostrarServicios(servicios){
         const servicioDiv = document.createElement('div');
         servicioDiv.classList.add('servicio');
         servicioDiv.dataset.idServicio = id;
+        servicioDiv.onclick = function (){
+            seleccionarServicio(servicio);
+        }
 
         servicioDiv.appendChild(nombreServicio);
         servicioDiv.appendChild(precioServicio);
@@ -134,4 +144,10 @@ function mostrarServicios(servicios){
         document.querySelector('#servicios').appendChild(servicioDiv);
         
     });
+}
+
+function seleccionarServicio(servicio){
+    const {servicios} = cita;
+    cita.servicios = [...servicios,servicio];
+    console.log(cita);
 }
