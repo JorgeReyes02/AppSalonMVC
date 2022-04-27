0f6317a32f1f8f9c2fca67ef2a0b6abcfcee40d6
Jorge Reyes Pacheco <jorgereyeslira02@gmail.com>
2022-04-26 21:17:59 -0500

    Consultando la Tabla de Joins con la informacion de citas

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,6 +1,7 @@
 <?php 
 namespace Controllers;
 
+use Model\AdminCita;
 use MVC\Router;
 
 class AdminController{
@@ -9,8 +10,29 @@ class AdminController{
         { 
             session_start(); 
         } 
+
+        //Consultar la base de Datos
+
+        $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
+        $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
+        $consulta .= " FROM citas  ";
+        $consulta .= " LEFT OUTER JOIN usuarios ";
+        $consulta .= " ON citas.usuarioId=usuarios.id  ";
+        $consulta .= " LEFT OUTER JOIN citasservcios ";
+        $consulta .= " ON citasservcios.citaId=citas.id ";
+        $consulta .= " LEFT OUTER JOIN servicios ";
+        $consulta .= " ON servicios.id=citasserv    cios.servicioId ";
+        // $consulta .= " WHERE fecha =  '${fecha}' ";
+        $citas = AdminCita::SQL($consulta);
+         echo '<pre>';
+         var_dump($citas);
+         echo '</pre>'; 
+        exit;
+
+
         $router->render('admin/index',[
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $_SESSION['nombre'],
+            'citas' => $citas
         ]);
     }
 }
